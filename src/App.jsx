5ced705a773d8a2de12d929f9647d6a032833f1f
SanguinatorX <romangurin01@gmail.com>
2026-06-04 22:32:34 +0200

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react';
-import './App.css';
 
 function App () {
   return (
     <div className='App'>
-      <h1>Bienvenue sur mon portfolio !</h1>
-      <p>Je suis un développeur passionné par la création de projets innovants et fonctionnels. Sur ce portfolio, vous trouverez une sélection de mes travaux les plus récents, ainsi que des informations sur mes compétences et mon parcours professionnel.</p>
-      <p>N'hésitez pas à parcourir les différentes sections pour en savoir plus sur mes projets et mes expériences. Si vous avez des questions ou souhaitez collaborer, n'hésitez pas à me contacter !</p>
+      <img src="/public/favicon.ico" alt="Favicon" />
+      <h1>Bienvenue sur le portfolio de <cite>SanguinatorX</cite> !</h1>
+      <p>
+        Je suis un développeur passionné par la création de projets innovants et fonctionnels.
+        Sur ce portfolio, vous trouverez une sélection de mes travaux les plus récents,
+        ainsi que des informations sur mes compétences et mon parcours professionnel.
+      </p>
+      <p>
+        N'hésitez pas à parcourir les différentes sections pour en savoir plus sur mes projets et mes expériences.
+        Si vous avez des questions ou souhaitez collaborer, n'hésitez pas à me contacter !
+      </p>
     </div>
   )
 }
