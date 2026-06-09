86462e1c54d250d18a28798c61b930c92a532210
SanguinatorX <romangurin01@gmail.com>
2026-06-09 17:32:50 +0200

    added escape button

--- src/realisations.jsx
+++ src/realisations.jsx
@@ -1,6 +1,12 @@
+import { Link } from "react-router-dom";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+
 function Webdev () {
   return (
     <section className="realisations">
+      <Link to="/" className="escapeButton"><FontAwesomeIcon icon={faArrowRightFromBracket} /></Link>
       <div className="webdev">
         <h2>Webdev</h2>
         <p>Ici je dev !</p>
@@ -12,6 +18,7 @@ function Webdev () {
 function Cybersecurite () {
     return (
     <section className="realisations">
+      <Link to="/" className="escapeButton"><FontAwesomeIcon icon={faArrowRightFromBracket} /></Link>
       <div className="cybersecurite">
         <h2>Cybersécurité</h2>
         <p>Ici je sécu !</p>
@@ -23,6 +30,7 @@ function Cybersecurite () {
 function Gaming () {
     return (
     <section className="realisations">
+      <Link to="/" className="escapeButton"><FontAwesomeIcon icon={faArrowRightFromBracket} /></Link>
       <div className="gaming">
         <h2>Gaming</h2>
         <p>Ici je joue !</p>
@@ -34,6 +42,7 @@ function Gaming () {
 function IA () {
     return (
     <section className="realisations">
+      <Link to="/" className="escapeButton"><FontAwesomeIcon icon={faArrowRightFromBracket} /></Link>
       <div className="IA">
         <h2>IA</h2>
         <p>Ici un peu d'ingénierie d'invite !</p>
@@ -45,6 +54,7 @@ function IA () {
 function Modelisation () {
     return (
     <section className="realisations">
+      <Link to="/" className="escapeButton"><FontAwesomeIcon icon={faArrowRightFromBracket} /></Link>
       <div className="modelisation">
         <h2>Modélisation 3D</h2>
         <p>Ici je modélise des assets 3D !</p>
@@ -56,6 +66,7 @@ function Modelisation () {
 function Litterature () {
     return (
     <section className="realisations">
+      <Link to="/" className="escapeButton"><FontAwesomeIcon icon={faArrowRightFromBracket} /></Link>
       <div className="litterature">
         <h2>Littérature</h2>
         <p>Ici je lis !</p>
