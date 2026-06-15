b34f997a0d70d3e98d3d7de252dfc00681ce9819
SanguinatorX <romangurin01@gmail.com>
2026-06-15 20:14:52 +0200

    changed propos icon

--- src/components/navMenu.jsx
+++ src/components/navMenu.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHouse, faIdCardClip, faCommentDots, faGear } from "@fortawesome/free-solid-svg-icons";
+import { faHouse, faIdCardClip, faCircleQuestion, faGear } from "@fortawesome/free-solid-svg-icons";
 
 function NavMenu (props) {
   return (
@@ -9,7 +9,7 @@ function NavMenu (props) {
       <ul>
         <li><Link to="/"><button><FontAwesomeIcon icon={faHouse} />&nbsp;Accueil</button></Link></li>
         <li><Link to="/contacts"><button><FontAwesomeIcon icon={faIdCardClip} />&nbsp;Contacter</button></Link></li>
-        <li><Link to="/propos"><button><FontAwesomeIcon icon={faCommentDots} />&nbsp;À propos</button></Link></li>
+        <li><Link to="/propos"><button><FontAwesomeIcon icon={faCircleQuestion} />&nbsp;À propos</button></Link></li>
         <li><Link to="/settings"><button><FontAwesomeIcon icon={faGear} />&nbsp;Paramètres</button></Link></li>
       </ul>
     </nav>
