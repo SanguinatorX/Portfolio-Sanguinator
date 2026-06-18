784054c734795334970aabadf99a7d2eaa884781
SanguinatorX <romangurin01@gmail.com>
2026-06-18 18:56:19 +0200

    added content to settings

--- src/components/settings.jsx
+++ src/components/settings.jsx
@@ -9,7 +9,23 @@ function Settings () {
             <Link to="/" className="escapeButton"><FontAwesomeIcon icon={faArrowRightFromBracket} /></Link>
             <form className="settingsBox">
                 <h1>Settings</h1>
-                <p>This is the settings page !</p>
+                <fieldset>
+                    <legend>Taille du texte</legend>
+                    <input type="range" min="6" max="24" step="1" defaultValue="16" />
+                </fieldset>
+                <fieldset>
+                    <legend>Couleur du texte</legend>
+                    <div className="colorator-wrap"><input type="color" defaultValue="#ffffff" /></div>
+                </fieldset>
+                <fieldset>
+                    <legend>Thème du site</legend>
+                    <select defaultValue="standard">
+                        <option value="standardTheme">Standard</option>
+                        <option value="greenTheme">Vert</option>
+                        <option value="redTheme">Rouge</option>
+                        <option value="yellowTheme">Jaune</option>
+                    </select>
+                </fieldset>
             </form>
         </div>
     );
